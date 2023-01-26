9374303f665afe7ff146e4e5a6dcd54a42651d59
Laura Ochoa <lauju28@hotmail.com>
2023-01-25 22:49:32 -0500

    fixed books route

--- app/books/routes.py
+++ app/books/routes.py
@@ -3,5 +3,5 @@ from werkzeug.routing import Rule
 books_urls = [
     Rule('/library/', endpoint='library/'),
     Rule('/library/book/<int:id>/', endpoint='library/book/'),
-    Rule('/library/book/<int:id>/page/<format>', endpoint='library/book/page/')
+    Rule('/library/book/<int:id>/page/<int:page>/<format>', endpoint='library/book/page/')
 ]
